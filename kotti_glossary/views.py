69533c1528704f675eccace77de8264d63227c90
Emmanuel Cazenave <emmanuel.cazenave@gmail.com>
2013-12-24 16:23:28 +0100

    Remove unnecessary class encapsulation

--- kotti_glossary/views.py
+++ kotti_glossary/views.py
@@ -4,61 +4,38 @@ from kotti.views.form import AddFormView
 from kotti.views.form import EditFormView
 from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config
-from pyramid.view import view_defaults
 
 from .resources import GlossDocument, Glossary, Term
 from .schemas import GlossarySchema, TermSchema
 from .utils import TermsProcessor
 
 
-@view_defaults(permission='edit')
-class NodeActions(object):
-    """Actions related to content nodes."""
-
-    def __init__(self, context, request):
-        self.context = context
-        self.request = request
-        self.flash = self.request.session.flash
-
-    def back(self, view=None):
-        """
-        Redirect to the given view of the context, the referrer of the request
-        or the default_view of the context.
-
-        :rtype: pyramid.httpexceptions.HTTPFound
-        """
-        url = self.request.resource_url(self.context)
-        if view is not None:
-            url += view
-        elif self.request.referrer:
-            url = self.request.referrer
-        return HTTPFound(location=url)
-
-    @view_config(name='scan-terms', permission='view',
-                 renderer='kotti_glossary:templates/scan-terms-view.pt')
-    def scan_terms(self):
-        """
-        """
-        if 'scan-terms' in self.request.POST:
-            body = self.context.body
-            proc = TermsProcessor(body)
-            _id = self.request.POST['glossary_id']
-            glossary = DBSession.query(Glossary).get(_id)
-            terms_dict = glossary.get_terms(self.request)
-            proc.reset_anchors()
-            self.context.body = proc.insert_anchors(terms_dict)
-            return HTTPFound(location=self.request.resource_url(self.context))
-        result = list()
-        for node in DBSession.query(Glossary):
-            result.append(node)
-        return dict(glossaries=result)
-
-    @view_config(name='reset-terms', permission='view')
-    def reset_terms(self):
-        body = self.context.body
+@view_config(name='scan-terms', permission='view',
+             renderer='kotti_glossary:templates/scan-terms-view.pt')
+def scan_terms(context, request):
+    """
+    """
+    if 'scan-terms' in request.POST:
+        body = context.body
         proc = TermsProcessor(body)
-        self.context.body = proc.reset_anchors()
-        return HTTPFound(location=self.request.resource_url(self.context))
+        _id = request.POST['glossary_id']
+        glossary = DBSession.query(Glossary).get(_id)
+        terms_dict = glossary.get_terms(request)
+        proc.reset_anchors()
+        context.body = proc.insert_anchors(terms_dict)
+        return HTTPFound(location=request.resource_url(context))
+    result = list()
+    for node in DBSession.query(Glossary):
+        result.append(node)
+    return dict(glossaries=result)
+
+
+@view_config(name='reset-terms', permission='view')
+def reset_terms(context, request):
+    body = context.body
+    proc = TermsProcessor(body)
+    context.body = proc.reset_anchors()
+    return HTTPFound(location=request.resource_url(context))
 
 
 @view_config(name='view', context=Glossary,
