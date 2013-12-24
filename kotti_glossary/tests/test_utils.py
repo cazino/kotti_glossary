efaaddba6a83e2c44234dc511b2625ae6756c6f9
Emmanuel Cazenave <emmanuel.cazenave@gmail.com>
2013-12-24 15:57:16 +0100

    Change utils API

--- kotti_glossary/tests/test_utils.py
+++ kotti_glossary/tests/test_utils.py
@@ -1,43 +1,7 @@
 import unittest
 
 
-class TestTransformTerms(unittest.TestCase):
-
-    def _getTargetClass(self):
-        from kotti_glossary.utils import TermsProcessor
-        return TermsProcessor
-
-    def _makeOne(self, html_text):
-        return self._getTargetClass()(html_text)
-
-    def test_p(self):
-        text = "<p>{{toto}}</p>"
-        tm = self._makeOne(text)
-        expected = '<p><a data-glossary-term="toto" href="">toto</a></p>'
-        self.assertEquals(expected, tm.transform_terms())
-
-    def test_no_tag(self):
-        text = "{{toto}}"
-        tm = self._makeOne(text)
-        expected = '<a data-glossary-term="toto" href="">toto</a>'
-        self.assertEquals(expected, tm.transform_terms())
-
-    def test_what(self):
-        text = u'<p>toto <strong>{{tata}}</strong></p>'
-        tm = self._makeOne(text)
-        expected = ('<p>toto <strong><a data-glossary-term="tata" href="">' +
-                    'tata</a></strong></p>')
-        self.assertEquals(expected, tm.transform_terms())
-
-    def test_space_after_term(self):
-        text = u'<p>toto {{tata}} aa</p>'
-        tm = self._makeOne(text)
-        expected = ('<p>toto <a data-glossary-term="tata" href="">' +
-                    'tata</a> aa</p>')
-        self.assertEquals(expected, tm.transform_terms())
-
-
-class TestApplyGlossary(unittest.TestCase):
+class TestInsertAnchors(unittest.TestCase):
 
     def _getTargetClass(self):
         from kotti_glossary.utils import TermsProcessor
@@ -47,12 +11,65 @@ class TestApplyGlossary(unittest.TestCase):
         return self._getTargetClass()(html_text)
 
     def test_basic(self):
-        text = '<p><a data-glossary-term="toto" href="">toto</a></p>'
+        text = 'toto'
+        tm = self._makeOne(text)
+        expected = '<a data-glossary-term="toto" href="/url">toto</a>'
+        self.assertEquals(expected, tm.insert_anchors(dict(toto="/url")))
+
+    def test_p(self):
+        text = "<p>toto</p>"
         tm = self._makeOne(text)
         expected = '<p><a data-glossary-term="toto" href="/url">toto</a></p>'
-        self.assertEquals(expected, tm.apply_glossary(dict(toto="/url")))
+        self.assertEquals(expected, tm.insert_anchors(dict(toto="/url")))
 
-    def test_raise_exec(self):
-        text = '<p><a data-glossary-term="toto" href="">toto</a></p>'
+    def test_multiple_markup(self):
+        text = u'<p>toto <strong>tata</strong></p>'
         tm = self._makeOne(text)
-        self.assertRaises(KeyError, tm.apply_glossary, dict())
+        expected = \
+            ('<p>toto <strong><a data-glossary-term="tata" href="/url">' +
+             'tata</a></strong></p>')
+        self.assertEquals(expected, tm.insert_anchors(dict(tata="/url")))
+
+    def test_space_after_term(self):
+        text = u'<p>toto tata aa</p>'
+        tm = self._makeOne(text)
+        expected = ('<p>toto <a data-glossary-term="tata" href="/url">' +
+                    'tata</a> aa</p>')
+        self.assertEquals(expected, tm.insert_anchors(dict(tata="/url")))
+
+
+class TestResetAnchors(unittest.TestCase):
+
+    def _getTargetClass(self):
+        from kotti_glossary.utils import TermsProcessor
+        return TermsProcessor
+
+    def _makeOne(self, html_text):
+        return self._getTargetClass()(html_text)
+
+    def test_basic(self):
+        text = '<a data-glossary-term="toto" href="/url">toto</a>'
+        tm = self._makeOne(text)
+        expected = 'toto'
+        self.assertEquals(expected, tm.reset_anchors())
+
+    def test_p(self):
+        text = '<p><a data-glossary-term="toto" href="/url">toto</a></p>'
+        expected = "<p>toto</p>"
+        tm = self._makeOne(text)
+        self.assertEquals(expected, tm.reset_anchors())
+
+    def test_multiple_markup(self):
+        text = \
+            ('<p>toto <strong><a data-glossary-term="tata" href="/url">' +
+             'tata</a></strong></p>')
+        expected = u'<p>toto <strong>tata</strong></p>'
+        tm = self._makeOne(text)
+        self.assertEquals(expected, tm.reset_anchors())
+
+    def test_space_after_term(self):
+        text = ('<p>toto <a data-glossary-term="tata" href="/url">' +
+                'tata</a> aa</p>')
+        tm = self._makeOne(text)
+        expected = u'<p>toto tata aa</p>'
+        self.assertEquals(expected, tm.reset_anchors())
--- kotti_glossary/utils.py
+++ kotti_glossary/utils.py
@@ -1,10 +1,8 @@
-import re
 from bs4 import BeautifulSoup
+import nltk
 
 
 class TermsProcessor(object):
-
-    regexp = re.compile("\{\{(\w*)\}\}")
 
     def __init__(self, html_text):
         self.html_text = html_text
@@ -21,41 +19,56 @@ class TermsProcessor(object):
         res = self.soup.new_string(text)
         return res
 
-    def _create_elems(self, token):
-        res = list()
-        end = len(token)
-        start = 0
-        matches = [m for m in self.regexp.finditer(token)]
-        if matches:
-            for match in matches:
-                match_start, match_end = match.span()
-                res.append(self._create_text(token[start:match_start]))
-                res.append(self._create_anchor(match.group(1)))
-                start = match_end
-            if start < end and start != 0:
-                res.append(self._create_text(token[start:]))
-        return res
+    def _is_gl(self, tag):
+        return tag.name == 'a' and tag.has_attr('data-glossary-term')
 
-    def transform_terms(self):
-        """Scan the text for term:xxx ocuurences and replace it with anchors
+    def _transform_term(self, term, term_lower, glossary):
+        anchor = self.soup.new_tag('a')
+        anchor['data-glossary-term'] = term_lower
+        anchor['href'] = glossary[term_lower]
+        anchor.append(term)
+        return anchor
+
+    def _create_anchors(self, text_soup, glossary):
+        result = list()
+        word_buffer = list()
+        match = False
+        for word in nltk.tokenize.word_tokenize(text_soup):
+            word_lower = word.lower()
+            if word_lower in glossary:
+                match = True
+                if word_buffer:
+                    result.append(
+                        self._create_text(" ".join(word_buffer) + " "))
+                    word_buffer = list()
+                result.append(self._transform_term(word, word_lower, glossary))
+            else:
+                word_buffer.append(word)
+        if word_buffer:
+            result.append(self._create_text(" " + " ".join(word_buffer)))
+        if not match:
+            result = list()
+        return result
+
+    def insert_anchors(self, glossary):
+        """Scan the glossary text for terms and replaces it with
+        anchors
         """
-        text_tokens = self.soup.find_all(text=True)
-        for token in text_tokens:
-            elems = self._create_elems(token)
+        text_soup_list = self.soup.find_all(text=True)
+        for text_soup in text_soup_list:
+            elems = self._create_anchors(text_soup, glossary)
             if elems:
-                parent = token.parent
-                token.extract()
+                parent = text_soup.parent
+                text_soup.extract()
                 for elem in elems:
                     parent.append(elem)
         return str(self.soup)
 
-    def _is_gl(self, tag):
-        return tag.name == 'a' and tag.has_attr('data-glossary-term')
-
-    def apply_glossary(self, glossary):
-        """glossary: dict of term: url
+    def reset_anchors(self):
+        """Replaces anchor tho the glossar by the raw term.
         """
-        for link in self.soup.find_all(self._is_gl):
-            term = link['data-glossary-term']
-            link['href'] = glossary[term]
+        for anchor in self.soup.find_all(self._is_gl):
+            text = self._create_text(anchor.text)
+            anchor.insert_before(text)
+            anchor.extract()
         return str(self.soup)
